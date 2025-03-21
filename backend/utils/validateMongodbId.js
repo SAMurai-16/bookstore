bbc304ce57c186ff547b74040cb6e8de96849164
SAMurai-16 <goforsamyak.c@gmail.com>
2025-03-22 02:48:52 +0530

    chl

--- backend/utils/validateMongodbId.js
+++ backend/utils/validateMongodbId.js
@@ -4,4 +4,4 @@ const validateMongoDbId = (_id)=>{
     if(!isValid) throw new Error("This Id is not valid or not found")
 }
 
-module.exports = validateMongoDbId
+module.exports = {validateMongoDbId}
