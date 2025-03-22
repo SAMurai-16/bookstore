2a157d3c9d5ea63de59170411245200deb75f6f8
Samyak Choudhary <162186916+SAMurai-16@users.noreply.github.com>
2025-03-22 14:32:01 +0530

    Update axiosConfig.js

--- my-app/src/utils/axiosConfig.js
+++ my-app/src/utils/axiosConfig.js
@@ -1,4 +1,4 @@
-export const base_url = "https://bookstore-q7d5.onrender.com//api/";
+export const base_url = "https://bookstore-q7d5.onrender.com/api/";
 
 const getTokenfromLocalStorage = localStorage.getItem("customer") 
     ? JSON.parse(localStorage.getItem("customer")) 
