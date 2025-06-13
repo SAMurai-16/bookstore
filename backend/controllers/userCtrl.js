6b3be347097c121f6f7de554b79911a852c6d54b
Samyak Choudhary <162186916+SAMurai-16@users.noreply.github.com>
2025-06-13 14:01:58 +0530

    Update userCtrl.js

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -74,7 +74,7 @@ const loginUserCtrl = asynchandler(async (req,res)=> {
 res.cookie("RefreshToken", RefreshToken, {
   httpOnly: true,
   secure: false, // ✅ set to true only if using HTTPS
-  sameSite: "lax", // ❗ 'lax' works for most local dev flows, 'none' if cross-site with HTTPS
+  sameSite: "none", // ❗ 'lax' works for most local dev flows, 'none' if cross-site with HTTPS
   maxAge: 72 * 60 * 60 * 1000,
 });
 
